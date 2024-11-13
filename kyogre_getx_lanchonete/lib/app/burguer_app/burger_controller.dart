ec868edbb85c027ecd709d65b4de321ac58d5773
Pedro Victor Veras <pedrovictor.rveras12@gmail.com>
2024-11-12 23:39:07 -0300

    up

--- kyogre_getx_lanchonete/lib/app/burguer_app/burger_controller.dart
+++ kyogre_getx_lanchonete/lib/app/burguer_app/burger_controller.dart
@@ -1,23 +1,22 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kyogre_getx_lanchonete/app/burguer_app/const/ingredientes_list_repository.dart';
 import 'package:kyogre_getx_lanchonete/app/burguer_app/entities/ingredientesModel.dart';
-import 'package:kyogre_getx_lanchonete/app/widgets/Custom/CustomText.dart';
 
 //!https://www.youtube.com/watch?v=ztEr9Bu8Dkc&t=124s -- 11:00
 
 // Controlador GetX para gerenciar a lista de ingredientes
-class BurgerController extends GetxController {
+class AppBurguerController extends GetxController {
   num burgerScale = 0.7;
   List<IngredientEntity> ingredients = <IngredientEntity>[];
 
   void init() {
     ingredients
-      ..add(getIngredientByType("bunBottom"))
-      ..add(getIngredientByType("bunTop"));
+      ..add(getIngredientByType("bunBottom") as IngredientEntity)
+      ..add(getIngredientByType("bunTop") as IngredientEntity);
   }
 
   void adicionarIngrediente(IngredientEntity ingrediente) {
-    ingrediente.insertIntoBurger = true;
+    ingrediente.insertIntBurger = true;
     ingredients.insert(ingredients.length - 1, ingrediente);
     animateIngrediente(ingrediente);
     update();
@@ -36,11 +35,11 @@ class BurgerController extends GetxController {
 
   IngredientEntity getIngredientByType(String type) {
     // Encontra o ingrediente com base no tipo na lista pré-definida
-    print(predefinedIngredientsList
-        .firstWhere((ingredient) => ingredient.type == type));
+    print(IngredientesListaRepository.firstWhere(
+        (ingredient) => ingredient.type == type));
 
-    return predefinedIngredientsList
-        .firstWhere((ingredient) => ingredient.type == type);
+    return IngredientesListaRepository.firstWhere(
+        (ingredient) => ingredient.type == type);
   }
 
   void animateAddTopBun() {
@@ -112,183 +111,5 @@ class BurgerController extends GetxController {
     }
 
     return totalHeight;
-  }
-}
-
-//todo -> ver o video no minuto 26:22 com a pagina
-class IngredientesScreen extends StatefulWidget {
-  const IngredientesScreen({super.key});
-
-  @override
-  State<IngredientesScreen> createState() => _IngredientesScreenState();
-}
-
-final burguerControll = Get.put(BurgerController()..init());
-
-class _IngredientesScreenState extends State<IngredientesScreen> {
-  bool animate = false;
-
-  @override
-  void setState(VoidCallback fn) {
-    // TODO: implement setState
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      setState(() {
-        burguerControll.ingredients.forEach((element) {
-          burguerControll.animateIngrediente(element);
-        });
-      });
-    });
-
-    super.initState;
-  }
-
-  void onAddIngrediente(IngredientEntity ingrediente) async {
-    setState(() {
-      burguerControll.animateRemoveTopBun();
-    });
-    await Future.delayed(
-      const Duration(milliseconds: 500),
-    );
-
-    setState(() {
-      burguerControll.addIngredient(ingrediente);
-    });
-    await Future.delayed(
-      const Duration(milliseconds: 500),
-    );
-    setState(() {
-      burguerControll.animateAddTopBun();
-    });
-  }
-
-  void onRemoveIngrediente(IngredientEntity ingrediente) async {
-    setState(() {
-      burguerControll.removeIngrediente(ingrediente);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.yellow,
-        body: SafeArea(
-          child: Column(
-            children: [
-              AnimatedSwitcher(
-                duration: const Duration(milliseconds: 500),
-                transitionBuilder: (child, animation) {
-                  return ScaleTransition(
-                    scale: animation,
-                    child: child,
-                  );
-                },
-                child: Text(
-                  "\$${burguerControll.totalPreco}",
-                  key: ValueKey(burguerControll.totalPreco),
-                  style: const TextStyle(
-                    fontSize: 50,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-
-              //Expanded
-              Expanded(
-                child: Center(
-                  child: Stack(
-                    clipBehavior: Clip.none,
-                    alignment: Alignment.center,
-                    children: [
-                      ...burguerControll.ingredients.map(
-                        (ingredient) => AnimatedPositioned(
-                          duration: const Duration(milliseconds: 780),
-                          curve: Curves.bounceOut,
-                          bottom: ingredient.insertIntoBurger
-                              ? burguerControll
-                                  .getIngredientesStackHeight(ingredient.type)
-                              : 0,
-                          top: burguerControll
-                              .getIngredientesStackHeight(ingredient.type),
-                          child: Draggable(
-                            feedback: Image.asset(ingredient.path,
-                                width: 50, height: 50, fit: BoxFit.cover),
-                            childWhenDragging: Container(),
-                            data: ingredient,
-                            child: Image.asset(ingredient.path,
-                                width: 50, height: 50, fit: BoxFit.cover),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          ),
-        ));
-  }
-}
-
-// Lista de ingredientes pré-definida para exemplo
-final predefinedIngredientsList = <IngredientEntity>[
-  IngredientEntity(
-    type: "bunBottom",
-    price: 0.5,
-    path: "assets/imagesBurguerApp/bottom.jpeg",
-  ),
-  IngredientEntity(
-      type: "bunTop", price: 0.5, path: "assets/imagesBurguerApp/top.jpeg"),
-  IngredientEntity(
-      type: "cheese", price: 0.5, path: "assets/imagesBurguerApp/cheese.jpeg"),
-  IngredientEntity(
-      type: "tomato", price: 0.4, path: "assets/imagesBurguerApp/tomato.jpeg"),
-  IngredientEntity(
-      type: "picanha",
-      price: 1.5,
-      path: "assets/imagesBurguerApp/picanha.jpeg"),
-];
-
-// Widget principal
-class IngredientesListWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Instancia o controlador
-    final IngredientesController controller = Get.put(IngredientesController());
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Ingredientes"),
-      ),
-      body: GetBuilder<IngredientesController>(
-        builder: (_) {
-          return ListView.builder(
-            itemCount: controller.ingredientesList.length,
-            itemBuilder: (context, index) {
-              final ingrediente = controller.ingredientesList[index];
-              return ListTile(
-                leading: Image.asset(
-                  ingrediente.path,
-                  width: 50,
-                  height: 50,
-                  fit: BoxFit.cover,
-                ),
-                title: Text(ingrediente.type),
-                subtitle: Text("Preço: \$${ingrediente.price}"),
-                trailing: IconButton(
-                  icon: Icon(
-                    ingrediente.insertIntBurger
-                        ? Icons.check_box
-                        : Icons.check_box_outline_blank,
-                  ),
-                  onPressed: () {
-                    controller.toggleInsertIntBurger(index);
-                  },
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
   }
 }
--- kyogre_getx_lanchonete/lib/app/burguer_app/entities/ingredientesModel.dart
+++ kyogre_getx_lanchonete/lib/app/burguer_app/entities/ingredientesModel.dart
@@ -1,102 +1,201 @@
 import 'package:get/get.dart';
 
+// Modelo Ingredientes
+// Ingredient Model
 class IngredientEntity {
   final String type;
   final double price;
   final String path;
+  final double height;
   bool insertIntoBurger;
-  final double height;
-
-  IngredientEntity(
-    this.height, {
-    required this.type,
-    required this.price,
-    required this.path,
-    this.insertIntoBurger = false,
-  });
-}
-
-// Modelo Ingredientes
-class Ingredientes {
-  final String type;
-  final double price;
-  final String path;
-  final double height;
-  final bool insertIntBurger;
   final num offset;
   final double scale;
   final int max;
 
-  Ingredientes({
+  IngredientEntity({
     required this.type,
     required this.price,
     required this.path,
     required this.height,
-    required this.insertIntBurger,
+    required this.insertIntoBurger,
     required this.offset,
     required this.scale,
     required this.max,
   });
+
+  void toggleInsert() {
+    insertIntoBurger = !insertIntoBurger;
+  }
+}
+
+// Burger Controller using GetX
+class BurgerController extends GetxController {
+  final ingredients = <IngredientEntity>[].obs;
+  double burgerScale = 1.0;
+  double totalPreco = 0.0;
+
+  void init() {
+    ingredients.addAll([
+      IngredientEntity(
+        type: 'bunTop',
+        price: 2.0,
+        path: 'assets/imagesBurguerApp/top.png',
+        height: 45,
+        insertIntoBurger: true,
+        offset: 0,
+        scale: 1,
+        max: 1,
+      ),
+      IngredientEntity(
+        type: 'bunBottom',
+        price: 2.0,
+        path: 'assets/imagesBurguerApp/bottom.png',
+        height: 45,
+        insertIntoBurger: true,
+        offset: 0,
+        scale: 1,
+        max: 1,
+      ),
+    ]);
+    calculateTotal();
+  }
+
+  void animateIngrediente(IngredientEntity ingredient) {
+    ingredient.insertIntoBurger = true;
+    update();
+  }
+
+  void animateRemoveTopBun() {
+    final topBun = ingredients.firstWhere((e) => e.type == 'bunTop');
+    topBun.insertIntoBurger = false;
+    update();
+  }
+
+  void animateAddTopBun() {
+    final topBun = ingredients.firstWhere((e) => e.type == 'bunTop');
+    topBun.insertIntoBurger = true;
+    update();
+  }
+
+  double getIngredientesStackHeight(dynamic type) {
+    double height = 0;
+    for (var ingredient in ingredients.where((e) => e.insertIntoBurger)) {
+      height += ingredient.height;
+    }
+    return height;
+  }
+
+  void addIngredient(IngredientEntity ingredient) {
+    if (!ingredientesInBurger(ingredient)) {
+      ingredients.add(ingredient.copyWith(insertIntoBurger: true));
+      calculateTotal();
+      update();
+    }
+  }
+
+  void removeIngrediente(IngredientEntity ingredient) {
+    ingredients
+        .removeWhere((e) => e.type == ingredient.type && e.insertIntoBurger);
+    calculateTotal();
+    update();
+  }
+
+  bool ingredientesInBurger(IngredientEntity ingredient) {
+    return ingredients
+            .where((e) => e.type == ingredient.type && e.insertIntoBurger)
+            .length >=
+        ingredient.max;
+  }
+
+  void calculateTotal() {
+    totalPreco = ingredients
+        .where((e) => e.insertIntoBurger)
+        .fold(0, (sum, item) => sum + item.price);
+  }
+}
+
+extension IngredientEntityExtension on IngredientEntity {
+  IngredientEntity copyWith({
+    String? type,
+    double? price,
+    String? path,
+    double? height,
+    bool? insertIntoBurger,
+    num? offset,
+    double? scale,
+    int? max,
+  }) {
+    return IngredientEntity(
+      type: type ?? this.type,
+      price: price ?? this.price,
+      path: path ?? this.path,
+      height: height ?? this.height,
+      insertIntoBurger: insertIntoBurger ?? this.insertIntoBurger,
+      offset: offset ?? this.offset,
+      scale: scale ?? this.scale,
+      max: max ?? this.max,
+    );
+  }
 }
 
 // Controlador GetX para gerenciar a lista de ingredientes
 class IngredientesController extends GetxController {
-  var ingredientesList = <Ingredientes>[
-    Ingredientes(
+  var ingredientesList = <IngredientEntity>[
+    IngredientEntity(
       height: 30,
       type: 'picanha',
       price: 15.0,
-      insertIntBurger: false,
+      insertIntoBurger: false,
       offset: 0,
       scale: 1,
       max: 1,
       path: 'assets/imagesBurguerApp/picanha.jpeg',
     ),
-    Ingredientes(
+    IngredientEntity(
       height: 20,
       type: 'queijo',
       price: 15.0,
-      insertIntBurger: false,
+      insertIntoBurger: false,
       offset: -50,
       scale: 1,
       max: 5,
       path: 'assets/imagesBurguerApp/queijo.jpeg',
     ),
-    Ingredientes(
+    IngredientEntity(
       height: 50,
       type: 'tomate',
       price: -15.0,
-      insertIntBurger: false,
+      insertIntoBurger: false,
       offset: 0,
       scale: 1,
       max: 1,
       path: 'assets/imagesBurguerApp/tomate.jpeg',
     ),
-    Ingredientes(
+    IngredientEntity(
       height: 45,
       type: 'picanha',
       price: 15.0,
-      insertIntBurger: false,
+      insertIntoBurger: false,
       offset: 0,
       scale: 1,
       max: 1,
       path: 'assets/imagesBurguerApp/picanha.jpeg',
     ),
-    Ingredientes(
+    IngredientEntity(
       height: 45,
       type: 'bottom',
       price: 15.0,
-      insertIntBurger: false,
+      insertIntoBurger: false,
       offset: 0,
       scale: 1,
       max: 1,
       path: 'assets/imagesBurguerApp/bottom.jpeg',
     ),
-    Ingredientes(
+    IngredientEntity(
       height: 45,
       type: 'top',
       price: 15.0,
-      insertIntBurger: false,
+      insertIntoBurger: false,
       offset: 0,
       scale: 1,
       max: 1,
@@ -105,11 +204,11 @@ class IngredientesController extends GetxController {
   ].obs;
 
   void toggleInsertIntBurger(int index) {
-    ingredientesList[index] = Ingredientes(
+    ingredientesList[index] = IngredientEntity(
       height: ingredientesList[index].height,
       type: ingredientesList[index].type,
       price: ingredientesList[index].price,
-      insertIntBurger: !ingredientesList[index].insertIntBurger,
+      insertIntoBurger: !ingredientesList[index].insertIntoBurger,
       offset: ingredientesList[index].offset,
       scale: ingredientesList[index].scale,
       max: ingredientesList[index].max,
